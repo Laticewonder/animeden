4f5d6a55e306c2c7931c0e7ae1830c2ca66a2751
Laticewonder <88091076+Laticewonder@users.noreply.github.com>
2022-10-17 14:12:21 +0530

    created navbar

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  images: {
+    domains: ["s4.anilist.co"]
+  },
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Feature from "../components/Feature";
+import Header from "../components/Header";
+
+const Home = () => {
+  return ( 
+    <div className="container">
+    <Header/>
+    <Feature/>
+    </div>
+    
+  );
+}
+ 
+export default Home;
