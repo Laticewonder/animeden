222cbe709dae775b18b6fd2aa0cb2732092e7d40
Laticewonder <88091076+Laticewonder@users.noreply.github.com>
2022-10-17 21:53:44 +0530

    update feature

--- components/Feature.js
+++ components/Feature.js
@@ -1,12 +1,12 @@
 import Image from "next/image";
 
 const Feature = () => {
-    return ( 
+    return (
         <div className="feature absolute top-20 left-0 right-0 lg:top-5 lg:left-28 w-11/12 lg:w-2/3 h-[188px] bg-slate-600 rounded-md truncate cursor-pointer flex mx-auto lg:ml-0 ">
-            <Image src='https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx127230-FlochcFsyoF4.png' height={188} width={140}/>
+            <Image src='https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx127230-FlochcFsyoF4.png' height={188} width={140} />
             <h1 className="text-white">feature</h1>
         </div>
-     );
+    );
 }
- 
+
 export default Feature;
